8166d76c6ab3ef6e1db1fa5afc733bce4d15404c
Kingcedru <byishimocedrick@gmail.com>
2024-07-05 17:59:48 +0200

    modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { PokemonBaseModule } from './pokemon-base/pokemon-base.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, PokemonBaseModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
