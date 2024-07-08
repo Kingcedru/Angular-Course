15582e571f2b1e5c6b23d9d5667d2fa08b369466
Kingcedru <byishimocedrick@gmail.com>
2024-07-08 11:46:20 +0200

    observables

--- src/app/pokemon-base/pokemon-base.module.ts
+++ src/app/pokemon-base/pokemon-base.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 import { PokemonService } from '../services/pokemon.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, PokemonListComponent, PokemonDetailComponent],
+  imports: [
+    CommonModule,
+    PokemonListComponent,
+    PokemonDetailComponent,
+    HttpClientModule,
+  ],
   exports: [PokemonListComponent, PokemonDetailComponent],
   providers: [PokemonService],
 })
